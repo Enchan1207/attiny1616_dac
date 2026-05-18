20fbfce106be7e141193ab8e6308aa2ea7986dc1
enchan <enchant0130@gmail.com>
2026-05-18 22:51:09 +0900

    feat: #4 ロータリエンコーダの回転方向によって波形を切り替えられるようにした

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <util/delay.h>
 
@@ -7,16 +8,32 @@
 #include "wavetable/pulse.h"
 #include "wavetable/sine.h"
 
+volatile int tableIndex = 0;
+
+ISR(PORTB_PORT_vect) {
+    PORTB.INTFLAGS = PIN0_bm;
+
+    // PC0を読んでCW/CCWを判断
+    if (PORTC.IN & 0b00000001) {
+        tableIndex = 1;
+    } else {
+        tableIndex = 2;
+    }
+}
+
 int main() {
     // クロック設定 (プリスケーラ1倍, 無効)
     _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, 0x00);
 
     PORTA.DIRSET = 1 << 6;
 
+    // PB0, PC0を入力に設定し、PB0の立下りエッジ割り込みを有効化
+    PORTB.DIRCLR = 1 << 0;
+    PORTC.DIRCLR = 1 << 0;
+    PORTB.PIN0CTRL = (PORTB.PIN0CTRL & ~PORT_ISC_gm) | (PORT_ISC_FALLING_gc << PORT_ISC_gp);
+
     oscillator_init();
 
-    // oscillator_set_wavetable(osc0, &pulseTable);
-    oscillator_set_wavetable(osc0, &sineTable);
     oscillator_set_frequency(osc0, 440);
 
     oscillator_enable(osc0);
@@ -24,6 +41,10 @@ int main() {
     sei();
 
     while (1) {
-        //
+        if (tableIndex > 0) {
+            const wavetable_t* ptr = tableIndex == 1 ? &sineTable : &pulseTable;
+            oscillator_set_wavetable(osc0, ptr);
+            tableIndex = 0;
+        }
     }
 }
