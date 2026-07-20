4e50fcc9be9cda83a44340f9e9fbc89136f141f8
enchan <enchant0130@gmail.com>
2026-07-20 22:41:51 +0900

    chore: オシレータのstepを直接設定するようにした

--- src/module/oscillator.c
+++ src/module/oscillator.c
@@ -15,9 +15,7 @@ void oscillator_set_wavetable(oscillator_ctx_t* ctx, const wavetable_t* table) {
     ctx->table = table;
 }
 
-void oscillator_set_frequency(oscillator_ctx_t* ctx, uint16_t frequency) {
-    // 周波数を65536分率に変換してステップとする
-    uint16_t step = (uint32_t)frequency * 65536UL / AUDIO_SAMPLE_RATE;
+void oscillator_set_step(oscillator_ctx_t* ctx, uint16_t step) {
     ctx->step = step;
 }
 
