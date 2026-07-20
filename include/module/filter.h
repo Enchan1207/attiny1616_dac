1fdbba107fbd4ffe5d654bbd4c3d1e5b1f2a222d
enchan <enchant0130@gmail.com>
2026-07-20 17:10:31 +0900

    feat: #7 一次LPFモジュールを実装

---
+++ include/module/filter.h
@@ -0,0 +1,33 @@
+#ifndef MODULE_FILTER_H
+#define MODULE_FILTER_H
+
+#include <stdint.h>
+
+#include "module/audio.h"
+
+/// @brief フィルタコンテキスト
+typedef struct {
+    int32_t state_q8;
+    uint8_t coefficient;
+} filter_ctx_t;
+
+/// @brief フィルタを初期化する
+/// @param ctx
+void filter_init(filter_ctx_t* ctx);
+
+/// @brief フィルタへ値を入力し、結果を得る
+/// @param ctx
+/// @return
+audio_sample_t filter_step(filter_ctx_t* ctx, audio_sample_t input);
+
+/// @brief フィルタのLPF係数を設定する
+/// @param ctx
+/// @param coefficient 係数 (0: カットオフ低 / 255: カットオフ高)
+void filter_set_coefficient(filter_ctx_t* ctx, uint8_t coefficient);
+
+/// @brief フィルタの内部状態を指定したサンプル値へリセットする
+/// @param ctx
+/// @param value
+void filter_reset(filter_ctx_t* ctx, audio_sample_t value);
+
+#endif /* MODULE_FILTER_H */
---
+++ src/module/filter.c
@@ -0,0 +1,23 @@
+#include "module/filter.h"
+
+void filter_init(filter_ctx_t* ctx) {
+    ctx->coefficient = 0;
+    ctx->state_q8 = 0;
+}
+
+audio_sample_t filter_step(filter_ctx_t* ctx, audio_sample_t input) {
+    int32_t input_q8 = (int32_t)input * 256;
+    int32_t diff = ctx->state_q8 - input_q8;
+
+    ctx->state_q8 += (diff * ctx->coefficient) >> 8;
+
+    return (audio_sample_t)(ctx->state_q8 >> 8);
+}
+
+void filter_set_coefficient(filter_ctx_t* ctx, uint8_t coefficient) {
+    ctx->coefficient = coefficient;
+}
+
+void filter_reset(filter_ctx_t* ctx, audio_sample_t value) {
+    ctx->state_q8 = (int16_t)value << 8;
+}
