edd6293b39bddb4a81836331eb61757f96f5e0bc
enchan <enchant0130@gmail.com>
2026-07-20 22:08:26 +0900

    chore: 全体のサンプルレートを定数で定義

--- include/module/audio.h
+++ include/module/audio.h
@@ -5,4 +5,7 @@
 
 typedef int8_t audio_sample_t;
 
+/// @brief オーディオ全体のサンプルレート
+extern const uint16_t AUDIO_SAMPLE_RATE;
+
 #endif /* MODULE_AUDIO_H */
---
+++ src/module/audio.c
@@ -0,0 +1,3 @@
+#include "module/audio.h"
+
+const uint16_t AUDIO_SAMPLE_RATE = 32000;
