9fca867d6152e2154e9ebb303164bf2263c0499d
enchan <enchant0130@gmail.com>
2026-07-20 22:08:50 +0900

    feat: #9 LFOの実体を実装

--- include/module/lfo.h
+++ include/module/lfo.h
@@ -1,4 +1,42 @@
 #ifndef MODULE_LFO_H
 #define MODULE_LFO_H
 
+#include <stdint.h>
+
+/// @brief LFO波形
+typedef enum {
+    LFO_WAVEFORM_SQUARE,
+    LFO_WAVEFORM_TRIANGLE,
+} lfo_waveform_t;
+
+/// @brief LFOコンテキスト
+typedef struct {
+    uint16_t phase;
+    uint16_t step;
+
+    uint8_t divider_counter;
+    int8_t current_output;
+
+    lfo_waveform_t waveform;
+} lfo_ctx_t;
+
+/// @brief LFOを初期化する
+/// @param ctx
+void lfo_init(lfo_ctx_t* ctx);
+
+/// @brief LFOの状態を進める
+/// @param ctx
+/// @return
+int8_t lfo_step(lfo_ctx_t* ctx);
+
+/// @brief LFOの波形を設定する
+/// @param ctx
+/// @param wfm
+void lfo_set_waveform(lfo_ctx_t* ctx, lfo_waveform_t wfm);
+
+/// @brief LFOの周波数を設定する
+/// @param ctx
+/// @param freq 周波数 (mHz)
+void lfo_set_frequency(lfo_ctx_t* ctx, uint16_t freq);
+
 #endif /* MODULE_LFO_H */
--- src/module/lfo.c
+++ src/module/lfo.c
@@ -1,1 +1,56 @@
 #include "module/lfo.h"
+
+#include "module/audio.h"
+
+const uint8_t LFO_DIVIDER = 32;
+
+void lfo_init(lfo_ctx_t* ctx) {
+    ctx->phase = 0;
+    ctx->step = 0;
+    ctx->divider_counter = 0;
+    ctx->current_output = 0;
+    ctx->waveform = LFO_WAVEFORM_SQUARE;
+}
+
+void lfo_set_waveform(lfo_ctx_t* ctx, lfo_waveform_t wfm) {
+    ctx->waveform = wfm;
+}
+
+void lfo_set_frequency(lfo_ctx_t* ctx, uint16_t freq) {
+    uint16_t step = (uint32_t)freq * 65536UL / AUDIO_SAMPLE_RATE;
+    ctx->step = step;
+}
+
+static inline int8_t calculate_lfo_output(lfo_ctx_t* ctx) {
+    switch (ctx->waveform) {
+        case LFO_WAVEFORM_TRIANGLE:
+            uint8_t phase = ctx->phase >> 8;
+
+            if (phase < 128) {
+                return (int8_t)((int16_t)phase * 2 - 128);
+            }
+
+            return (int8_t)(127 - ((int16_t)(phase - 128) * 2));
+
+        case LFO_WAVEFORM_SQUARE:
+            return (ctx->phase & 0x8000) ? -127 : 127;
+
+        default:
+            return 0;
+    }
+}
+
+int8_t lfo_step(lfo_ctx_t* ctx) {
+    // LFO_DIVIDER回呼ばれるまで前の値を返し続ける
+    ctx->divider_counter++;
+    if (ctx->divider_counter < LFO_DIVIDER) {
+        return ctx->current_output;
+    }
+    ctx->divider_counter = 0;
+
+    // 値を計算してphaseを進める
+    int8_t value = calculate_lfo_output(ctx);
+    ctx->phase += ctx->step;
+    ctx->current_output = value;
+    return value;
+}
